adfe639a62b604cadce00cbdba3f5786eec3b4de
ishkhaniantatevik <58734293+ishkhaniantatevik@users.noreply.github.com>
2020-08-26 20:56:08 +0000

    Create BubbleSorting.cs

---
+++ SortingAlgorithms/BubbleSorting.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+namespace SortingAlgorithms
+{
+    public static class BubbleSorting
+    {
+        public static int[] BubbleSort(int[] unsortedArray)
+        {
+            int temp;
+            for (int i = 0; i < unsortedArray.Length - 2; ++i)
+            {
+                for (int j = 0; j < unsortedArray.Length - 2; ++j)
+                {
+                    if (unsortedArray[i] > unsortedArray[i + 1])
+                    {
+                        temp = unsortedArray[i + 1];
+                        unsortedArray[i + 1] = unsortedArray[i];
+                        unsortedArray[i] = temp;
+                    }
+                }
+                
+            }
+            
+            return unsortedArray;
+        }
+
+       
+
+    }
+}
