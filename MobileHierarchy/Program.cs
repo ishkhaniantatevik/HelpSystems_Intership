3d53f276fc6aecbd71cea57071b3f32046d309c0
ishkhaniantatevik <58734293+ishkhaniantatevik@users.noreply.github.com>
2020-09-01 20:55:50 +0000

    Update Program.cs

--- MobileHierarchy/Program.cs
+++ MobileHierarchy/Program.cs
@@ -1,1 +1,27 @@
 
+using System;
+
+namespace MobileHierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobile Iphone9 = new Iphone();
+            Iphone9.Display();
+            Iphone9.Call();
+            Iphone9.TakePhoto();
+            
+            Mobile RedmiNote95 = new Xiaomi();
+            RedmiNote95.Display();
+            RedmiNote95.Call();
+            RedmiNote95.TakePhoto();
+            
+            Mobile NokiaX6 = new Nokia();
+            NokiaX6.Display();
+            NokiaX6.Call();
+            NokiaX6.TakePhoto();
+            
+        }
+    }
+}
